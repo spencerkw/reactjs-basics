7f20716c04660b341e0151ee8fa63bf46b8f9a6d
Spencer K-W <keashlywarters@gmail.com>
2019-07-11 15:47:13 -0400

    First Component

--- src/index.js
+++ src/index.js
@@ -1,1 +1,20 @@
-console.log("It works!");
+import React from "react";
+import { render } from "react-dom";
+
+//root component
+class App extends React.Component {
+  render() {
+    return (
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-10 col-xs-offset-1">
+            <h1>Hello!</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+//renders the base App component in place of the #app div in the HTML
+render(<App />, window.document.getElementById("app"));
