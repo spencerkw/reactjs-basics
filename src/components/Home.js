0e3012decc5339bab122e8eb2f852dbf2bd9ece1
Spencer K-W <keashlywarters@gmail.com>
2019-07-11 16:38:03 -0400

    Events & ReactJS

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,22 +2,23 @@ import React from "react";
 import PropTypes from "prop-types";
 
 export class Home extends React.Component {
+  constructor(props) {
+    super();
+    this.age = props.age;
+  }
+
+  onMakeOlder() {
+    this.age += 3;
+    console.log(this.age);
+  }
+
   render() {
-    let text = "Something!";
     return (
       <div>
         <p>In a new Component!</p>
-        <p>{text}</p>
-        <p>Your name is {this.props.name}, your age is {this.props.age}</p>
-        <p>User Object => Name: {this.props.user.name}</p>
-        <div>
-          <h4>Hobbies</h4>
-          <ul>
-            {this.props.user.hobbies.map((hobby, i) => <li key={i}>{hobby}</li>)}
-          </ul>
-        </div>
-        <hr/>
-        {this.props.children}
+        <p>Your name is {this.props.name}, your age is {this.age}</p>
+        <hr />
+        <button onClick={() => this.onMakeOlder()} className="btn btn-primary">Make me older!</button>
       </div>
     );
   }
@@ -26,7 +27,5 @@ export class Home extends React.Component {
 //different from tutorial: have to use the extra PropTypes library
 Home.propTypes = {
   name: PropTypes.string,
-  age: PropTypes.number,
-  user: PropTypes.object,
-  children: PropTypes.element.isRequired
+  age: PropTypes.number
 }
--- src/index.js
+++ src/index.js
@@ -7,10 +7,6 @@ import { Home } from "./components/Home";
 //root component
 class App extends React.Component {
   render() {
-    let user = {
-      name: "Anna",
-      hobbies: ["Sports", "Reading"]
-    };
     return (
       <div className="container">
         <div className="row">
@@ -18,9 +14,7 @@ class App extends React.Component {
             <Header />
           </div>
           <div className="col-xs-10 col-xs-offset-1">
-            <Home name={"Max"} age={27} user={user}>
-              <p>This is a paragraph!</p>
-            </Home>
+            <Home name={"Max"} age={27} />
           </div>
         </div>
       </div>
